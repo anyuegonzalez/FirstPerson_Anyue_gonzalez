a619097209d04fa90db0cf87457edb1a5543bbf6
anyuegonzalez <anyuee07@gmail.com>
2024-10-17 21:07:30 +0200

    gerg

--- Assets/Scripts/FirstPerson.cs
+++ Assets/Scripts/FirstPerson.cs
@@ -18,11 +18,27 @@ public class FirstPerson : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal"); 
         float v = Input.GetAxisRaw("Vertical");
 
-        //Vector3 movimiento = new Vector3(h, 0, v ).normalized;
 
+        //Vector3 movimiento = new Vector3(h, 0, v ).normalized;
+        
         Vector2 input = new Vector2(h, v).normalized;
 
-        //controller.Move(movimiento * velocidadMovimiento * Time.deltaTime);
+        // si existe input...
+        if(input.magnitude > 0)
+        {
+            // mse calcula el angulo al q tengo q rotarle en funcion de los input y en fuincion hacia donde este orientada la camara
+            float anguloRotcion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+
+            transform.eulerAngles = new Vector3(0, anguloRotcion, 0);
+
+            Vector3 movimiento = Quaternion.Euler(0, anguloRotcion, 0) * Vector3.forward;
+            
+            controller.Move(movimiento * velocidadMovimiento * Time.deltaTime);
+        } 
+
+
+
+       
 
 
     }
